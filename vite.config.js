5d523e7a7f4500bd187e8d54b27d24a5b4ef80fd
Ayush-23Y <2024.ayush.yadav@ves.ac.in>
2026-08-19 23:27:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base:'/FSD_Experiment_1',
+  base: '/FSD_Experiment_1/',
 })
