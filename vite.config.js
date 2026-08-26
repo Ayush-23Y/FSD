e1540104f3fda4ecd7e49a67c4bc96f786afd428
Ayush-23Y <2024.ayush.yadav@ves.ac.in>
2026-08-26 19:00:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/FSD/',
   plugins: [tailwindcss()],
-  base: '/FSD_Experiment_1/',
 })
